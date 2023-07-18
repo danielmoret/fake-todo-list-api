47b9f2ea987d3e22198a525eef0a92a76182a1cf
danielmoret <dmoret17@gmail.com>
2023-07-18 00:51:29 -0400

    todolist done

--- src/app.py
+++ src/app.py
@@ -2,6 +2,7 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 import os
+import json
 from flask import Flask, request, jsonify, url_for
 from flask_migrate import Migrate
 from flask_swagger import swagger
@@ -9,10 +10,14 @@ from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
 from models import db, User, Todo
+
 # from models import Person
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
+
+
+todo_file_path = os.path.join(os.path.dirname(__file__), 'todos.json')
 
 db_url = os.getenv("DATABASE_URL")
 if db_url is not None:
@@ -29,13 +34,23 @@ setup_admin(app)
 
 # Handle/serialize errors like a JSON object
 
+todo_file_path = os.path.join(os.path.dirname(__file__), 'todos.json')
+
+def save_todos(todos):
+    with open(todo_file_path, 'w') as f:
+        json.dump(todos, f, indent=4)
+
+def read_todos():
+    with open(todo_file_path, 'r') as f:
+        return json.load(f)
 
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
 
 # generate sitemap with all your endpoints
-
+def is_valid_task(task):
+    return isinstance(task.get("label"), str) and isinstance(task.get("done"), bool)
 
 @app.route('/')
 def sitemap():
@@ -44,102 +59,77 @@ def sitemap():
 
 @app.route('/todos/user', methods=['GET'])
 def get_all_users():
-    users = User.query.all()
-
-    users = list(map(lambda item: item.serialize(), users))
-    new_users = []
-
-    for user in users:
-        new_users.append(user.get("username"))
-    return jsonify(new_users), 200
+    todos = read_todos()
+    users = [todo["username"] for todo in todos]
+    return jsonify(users), 200
 
 
 @app.route('/todos/user/<string:username>', methods=['POST'])
 def create_user(username=None):
-    try:
-        data = request.json
-    except Exception as error:
-        return jsonify({"message": "You must add an empty array in the body of the request"}), 500
-
-    if type(data) == list and len(data) == 0:
-        user = User.query.filter_by(username=username).first()
-        if user is not None:
-            return jsonify({"msg": f"The user {username} already exists"}), 400
-        if user is None:
-            user = User(username=username)
-            db.session.add(user)
-            try:
-                db.session.commit()
-
-                todos = Todo(label="sample task",
-                             done=False, user_id=user.id)
-                db.session.add(todos)
-                db.session.commit()
-                return jsonify([]), 201
-            except Exception as error:
-                print(error)
-                return jsonify({"msg": error.args})
-    else:
+   
+   data = request.json
+   print(type(data))
+   if type(data) == list and len(data) == 0:
+        todos = read_todos()
+        for todo in todos:
+            if todo["username"] == username:
+                return jsonify({"msg": f"The user {username} already exists"}), 400
+            
+        new_user = {"username": username, "todos": [{"label": "sample task", "done": False}]}
+        todos.append(new_user)
+        save_todos(todos)
+        return jsonify([]), 201
+   else:
         return jsonify({"message": "You must add an empty array in the body of the request"}), 500
 
 
 @app.route('/todos/user/<string:username>', methods=['GET'])
 def get_all_todo(username=None):
-    if username is None:
-        return jsonify({"msg": "bad request"}), 400
+    todos= read_todos()
 
-    user = User.query.filter_by(username=username).one_or_none()
-    if user is None:
-        return jsonify({"msg": f"The user {username} doesn't exists"}), 400
-    if user is not None:
-        todos = Todo.query.filter_by(user_id=user.id).all()
-        if todos is None:
-            return jsonify([]), 200
-        if todos is not None:
-            return jsonify([todo.serialize() for todo in todos]), 200
+    user_todos = [todo["todos"] for todo in todos if todo["username"] == username]
+
+    if not user_todos:
+        return jsonify({"msg": f"The user {username} doesn't exist"}), 404
+    return jsonify(user_todos), 200
 
 
 @app.route('/todos/user/<string:username>', methods=['PUT'])
 def update_task(username=None):
-    user = User.query.filter_by(username=username).first()
-    if user is None:
-        return jsonify({"msg": f"The user {username} doesn't exists"}), 404
-
-    todos = Todo.query.filter_by(user_id=user.id).all()
-    for todo in todos:
-        db.session.delete(todo)
-    db.session.commit()
-
     data = request.json
-    if len(data) >= 1:
-        for todo in data:
 
-            todo = Todo(label=todo["label"],
-                        done=todo["done"], user_id=user.id)
-            db.session.add(todo)
-        try:
-            db.session.commit()
-            return jsonify({"msg": f"{len(data)} tasks were added successfully"}), 201
-        except Exception as error:
-            db.session.rollback()
-            return jsonify({"msg": error.args})
-    else:
-        return jsonify({"msg": "You must send at least one task"}), 400
+    if not data or not isinstance(data, list):
+        return jsonify({"msg": "You must send a valid JSON array of tasks"}), 400
+    
+    for task in data:
+        if not is_valid_task(task):
+            return jsonify({"msg": "Each task must have 'label' (string) and 'done' (boolean) properties"}), 400
+    
+    todos = read_todos()
+    user_todos = [todo for todo in todos if todo["username"] == username]
+
+    if not user_todos:
+        return jsonify({"msg": f"The user {username} doesn't exist"}), 404
+    
+    user_todos[0]["todos"] = data
+    save_todos(todos)
+
+    return jsonify({"msg": f"{len(data)} todos have been updated successfully"}),200
+
 
 
 @app.route('/todos/user/<string:username>', methods=['DELETE'])
 def delete_task(username=None):
-    if username is None:
-        return jsonify({"msg": "You must include a username in the URL of the request"}), 400
-
-    user = User.query.filter_by(username=username).first()
-    if user is None:
+    todos = read_todos()
+    user_index = [i for i, todo in enumerate(todos) if todo["username"] == username]
+   
+    if not user_index:
         return jsonify({"msg": f"The user {username} doesn't exist"}), 404
 
-    db.session.delete(user)
-    db.session.commit()
-    return jsonify({"msg": f"The user {username} has been deleted successfully"}), 200
+    todos.pop(user_index[0])
+    save_todos(todos)
 
+    return jsonify({"msg": f"The user {username} has been deleted successfully"}), 200
 
     # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
